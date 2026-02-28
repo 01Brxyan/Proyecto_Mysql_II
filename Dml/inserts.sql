465eba33258da99d04f11d201d69c9457869213d
Brayan Snehyder Castro Velandia <222234032+01Brxyan@users.noreply.github.com>
2026-02-28 22:24:41 +0000

    feat: ✨ Agregacion de Inserts

---
+++ Dml/inserts.sql
@@ -0,0 +1,8 @@
+INSERT INTO agentes (nombre,telefono,email)
+VALUES ('Pedro Ruiz','300111','pedro@mail.com');
+
+INSERT INTO tipos_propiedad (nombre_tipo)
+VALUES ('Casa'),('Apartamento'),('Local');
+
+INSERT INTO clientes (nombre,telefono,email)
+VALUES ('Juan Perez','300333','juan@mail.com');
